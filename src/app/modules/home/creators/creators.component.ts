db95e3c15432e479009ca93b972b4a7c280e93f6
FherEnrique <enquique500@gmail.com>
2020-09-21 20:52:31 -0600

    AboutUs falta estilo y app

--- src/app/modules/home/creators/creators.component.ts
+++ src/app/modules/home/creators/creators.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'gf-creators',
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class CreatorsComponent implements OnInit {
 
   constructor() { }
+  @Input() dev;
 
   ngOnInit(): void {
   }
-
 }
